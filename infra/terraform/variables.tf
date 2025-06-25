2ab70f9e32fc292a01367fb1f076fdba904e453f
brsavii <brunasavi47@gmail.com>
2025-06-25 15:46:18 -0300

    fix deploy

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,4 +1,14 @@
-variable "project_id" { type = string }
-variable "region"     { type = string, default = "southamerica-east1" }
-variable "service_name" { type = string, default = "strapi-cms" }
-variable "image"      { type = string }
+variable "project_id" {
+  type = string
+}
+variable "region" {
+  type    = string
+  default = "southamerica-east1"
+}
+variable "service_name" {
+  type    = string
+  default = "strapi-cms"
+}
+variable "image" {
+  type = string
+}
