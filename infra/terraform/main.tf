0998a581cffea37cb2291bbb5a311f50cbd881e4
brsavii <brunasavi47@gmail.com>
2025-06-25 16:40:38 -0300

    fix deploy

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,19 +1,34 @@
-# Cloud Run
+# Cloud Run Service: Strapi CMS
 resource "google_cloud_run_service" "strapi" {
   name     = var.service_name
   location = var.region
 
   template {
     spec {
-      # usa a SA que você já criou manualmente
+      # Usa a Service Account já criada manualmente
       service_account_name = "strapi-sa@${var.project_id}.iam.gserviceaccount.com"
 
       containers {
         image = var.image
+
+        # Porta exposta pelo container
         ports {
           container_port = 1337
         }
+
+        # Garante que o Strapi leia a porta certa e escute em todas as interfaces
+        env {
+          name  = "PORT"
+          value = "1337"
+        }
+        env {
+          name  = "HOST"
+          value = "0.0.0.0"
+        }
       }
+
+      # Aumenta o tempo máximo de inicialização (em segundos)
+      timeout_seconds = 300
     }
   }
 
@@ -23,7 +38,7 @@ resource "google_cloud_run_service" "strapi" {
   }
 }
 
-# Permitir invocação pública (allUsers)
+# Permitir invocação pública pelo allUsers
 resource "google_cloud_run_service_iam_member" "invoker" {
   service  = google_cloud_run_service.strapi.name
   location = google_cloud_run_service.strapi.location
